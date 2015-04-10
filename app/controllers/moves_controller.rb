25c0180e4464b653d001c1965cfdf11cecb0b302
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 02:45:37 -0400

    deployment error fix

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -4,12 +4,16 @@ require 'net/https'
 
 class MovesController < ApplicationController
 	def getAuth
-		redirect_to "https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id=' + 'rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl&scope=activity"
+		redirect_to "https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id=' +
+		 'rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl&scope=activity"
 	end
 
 	def receiveAuth
 		@code = params[:code]
 		
+	end
+
+	def helper
 		uri = URI.parse('https://api.moves-app.com/oauth/v1/access_token?' + 
 			'grant_type=authorization_code&code=' + @code + '&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl' +
 			'&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW')
@@ -23,7 +27,6 @@ class MovesController < ApplicationController
 			 {'Content-Type' =>'application/json'})
 
 		@res = http.request(request)
-
 	end
 
 end
