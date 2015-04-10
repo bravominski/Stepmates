059ce0e18686d47f0dae411d7bdb5471ed1fd82f
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 12:13:43 -0400

    merged sessions/authen/moves api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
   def new
-  end
+    if User.find_by(id: session[:user_id]) != nil 
+      redirect_to '/patients'
+    end
+  end 
 
   def create
   	 user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to '/patients'
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
