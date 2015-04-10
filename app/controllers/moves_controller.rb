c07543eba4ab1d84d7ce6b067332fbcb1e9c43fb
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 02:38:53 -0400

    http post request fixed

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -4,30 +4,22 @@ require 'net/https'
 
 class MovesController < ApplicationController
 	def getAuth
-		redirect_to "https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl&scope=activity"
+		redirect_to "https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id=' + 'rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl&scope=activity"
 	end
 
 	def receiveAuth
 		@code = params[:code]
 		
-		logger.info 'before parsing url'
+		uri = URI.parse('https://api.moves-app.com/oauth/v1/access_token?' + 
+			'grant_type=authorization_code&code=' + @code + '&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl' +
+			'&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW')
 
-		uri = URI.parse("https://api.moves-app.com/oauth/v1/access_token?grant_type=" + 
-			"authorization_code&code=" + code + "&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl" + 
-			"&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW")
-		
-		logger.info 'after parsing url'
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
 
-		https = Net::HTTP.new(uri.host,uri.port)
-		
-		logger.info 'after new http req'
-		https.use_ssl = true
+		request = Net::HTTP::Post.new(uri.path, {'Content-Type' =>'application/json'})
 
-		req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-		logger.info 'create a post req'
-
-		@res = https.request(req)
-		logger.info 'send post req'
+		@res = http.request(request)
 
 	end
 
