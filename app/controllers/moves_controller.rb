73ca6ecb03364f25a6068328ca05c240d59d936c
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 02:41:32 -0400

    http post request fixed

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -17,7 +17,10 @@ class MovesController < ApplicationController
 		http = Net::HTTP.new(uri.host, uri.port)
 		http.use_ssl = true
 
-		request = Net::HTTP::Post.new(uri.path, {'Content-Type' =>'application/json'})
+		request = Net::HTTP::Post.new('https://api.moves-app.com/oauth/v1/access_token?' + 
+			'grant_type=authorization_code&code=' + @code + '&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl' +
+			'&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW',
+			 {'Content-Type' =>'application/json'})
 
 		@res = http.request(request)
 
