bca9e6f9818d56cfebbc7ab4d1e1a7e99a1e8ac6
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 02:25:30 -0400

    trying to make a post request

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -10,6 +10,8 @@ class MovesController < ApplicationController
 	def receiveAuth
 		@code = params[:code]
 		
+		logger.info 'before parsing url'
+
 		uri = URI.parse("https://api.moves-app.com/oauth/v1/access_token?grant_type=" + 
 			"authorization_code&code=" + code + "&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl" + 
 			"&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW")
