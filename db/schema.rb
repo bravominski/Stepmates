00c87ffc45814393bd6b2843c8205003fec183af
rextardus <minski@sas.upenn.edu>
2015-04-10 11:00:25 -0400

    Authentication completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410141724) do
+ActiveRecord::Schema.define(version: 20150410145141) do
 
   create_table "patients", force: :cascade do |t|
     t.string   "name"
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20150410141724) do
     t.string   "name"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
