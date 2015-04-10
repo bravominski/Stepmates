4177f8ba2f70d397d218d33c86928963fb7960cc
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 02:48:32 -0400

    bad request fix #1

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -11,9 +11,6 @@ class MovesController < ApplicationController
 	def receiveAuth
 		@code = params[:code]
 		
-	end
-
-	def helper
 		uri = URI.parse('https://api.moves-app.com/oauth/v1/access_token?' + 
 			'grant_type=authorization_code&code=' + @code + '&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl' +
 			'&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW')
@@ -27,6 +24,10 @@ class MovesController < ApplicationController
 			 {'Content-Type' =>'application/json'})
 
 		@res = http.request(request)
+	end
+
+	def helper
+
 	end
 
 end
