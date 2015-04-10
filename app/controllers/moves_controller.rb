5c4ec17f3a563e32363f92efa0f1dc6fe30f52fb
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 04:42:55 -0400

    fetching fitness data working

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -12,15 +12,15 @@ class MovesController < ApplicationController
 	def receiveAuth
 		@code = params[:code]
 		
-		uri = URI.parse('https://api.moves-app.com')
+		uri = URI.parse("https://api.moves-app.com")
 
 		http = Net::HTTP.new(uri.host, uri.port)
 		http.use_ssl = true
 		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-		request = Net::HTTP::Post.new('/oauth/v1/access_token?' + 
-			'grant_type=authorization_code&code=' + @code + '&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl' +
-			'&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW')
+		request = Net::HTTP::Post.new("/oauth/v1/access_token?" + 
+			"grant_type=authorization_code&code=" + @code + "&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl" +
+			"&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW")
 
 		@res = http.request(request)
 		@res = @res.body
@@ -31,15 +31,15 @@ class MovesController < ApplicationController
 	end
 
 	def getData
-		@token = params[:access_token]
+		access_token = params[:access_token]
 
-		uri = URI.parse("https://api.moves-app.com/api/1.1")
+		uri = URI.parse("https://api.moves-app.com/api/1.1/user/activities/daily/20150401?" + 
+			"access_token=" + access_token)
 		http = Net::HTTP.new(uri.host, uri.port)
 		http.use_ssl = true
-
-		request = Net::HTTP::Get.new("/user/activities/daily/20150401?access_token=" + @token)
-
+		request = Net::HTTP::Get.new(uri.request_uri)
 		response = http.request(request)
+
 		@body = response.body
 
 	end
--- test.rb
+++ test.rb
@@ -1,6 +1,13 @@
-require 'open-uri'
-require 'json'
+require 'net/http'
 
-value = '{"val":"test","val1":"test1","val2":"test2"}'
-test = JSON.parse(value)["val"]
-puts test
+require "net/https"
+require "uri"
+
+uri = URI.parse("https://api.moves-app.com/api/1.1/user/activities/daily/20150401?access_token=e7GB8VDBNA62w4d0j6XalVQls62CxLvWueP2oO2mhjMwzVeqp9U5zLJ8Lc0ZEjAN")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(uri.request_uri)
+
+response = http.request(request)
+puts response.body
