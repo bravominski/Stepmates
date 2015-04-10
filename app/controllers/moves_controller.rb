8a4d49570e38d6cfdc9c8cf87b1abcf6b50f6005
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 02:59:48 -0400

    bad request fix #2

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -19,8 +19,7 @@ class MovesController < ApplicationController
 
 		request = Net::HTTP::Post.new('/oauth/v1/access_token?' + 
 			'grant_type=authorization_code&code=' + @code + '&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl' +
-			'&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW',
-			 {'Content-Type' =>'application/json'})
+			'&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW')
 
 		@res = http.request(request)
 	end
