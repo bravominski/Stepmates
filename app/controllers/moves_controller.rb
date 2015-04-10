75ce3733eb8b789cc10ec8b64215ea32e6144416
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 02:09:40 -0400

    http post req for access token

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -10,20 +10,15 @@ class MovesController < ApplicationController
 	def receiveAuth
 		@code = params[:code]
 
-
-		
-
-	end
-
-	def helper
 		uri = URI.parse("https://api.moves-app.com/oauth/v1/access_token?grant_type=" + 
 			"authorization_code&code=" + code + "&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl" + 
-			"&client_secret=8x0b3qdW0vlymVotk5njmJnaSX2Qd9veXP970M5nrTZjHGdKGb95V64x10eW51DW&")
+			"&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW")
 		https = Net::HTTP.new(uri.host,uri.port)
 		https.use_ssl = true
 		req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-		#req['foo'] = 'bar'
-		#req.body = "[ #{@toSend} ]"
+		
 		@res = https.request(req)
+
 	end
+
 end
