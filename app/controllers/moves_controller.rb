437b309fcc225a4d9b0399ab8baa5ab3030d5680
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-10 02:23:57 -0400

    trying to make a post request

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -9,15 +9,23 @@ class MovesController < ApplicationController
 
 	def receiveAuth
 		@code = params[:code]
-
+		
 		uri = URI.parse("https://api.moves-app.com/oauth/v1/access_token?grant_type=" + 
 			"authorization_code&code=" + code + "&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl" + 
 			"&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW")
-		https = Net::HTTP.new(uri.host,uri.port)
-		https.use_ssl = true
-		req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
 		
+		logger.info 'after parsing url'
+
+		https = Net::HTTP.new(uri.host,uri.port)
+		
+		logger.info 'after new http req'
+		https.use_ssl = true
+
+		req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+		logger.info 'create a post req'
+
 		@res = https.request(req)
+		logger.info 'send post req'
 
 	end
 
