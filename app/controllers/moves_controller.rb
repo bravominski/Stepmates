4e48a6dfbda257b5a20f7394d926add72d5f9fd0
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-15 14:48:53 -0400

    extracting access/refresh tokens in patient new page

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -27,6 +27,7 @@ class MovesController < ApplicationController
 
 		@access_token = JSON.parse(@res)["access_token"]
 		@refresh_token = JSON.parse(@res)["refresh_token"]
+		
 		render 'patients/new'
 
 	end
