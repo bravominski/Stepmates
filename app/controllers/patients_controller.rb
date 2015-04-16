745b73cced5015c963a609c899cbd7c04ce7267d
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-16 02:38:16 -0400

    added functionality to refresh token if one has expired

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -44,10 +44,47 @@ class PatientsController < ApplicationController
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
 
+    ###### this is our data that we received ######
     @body = response.body
-
     @parsed_data = JSON.parse(@body)
 
+
+    ###### if our access token has been expired, we need to refresh it ######
+    if parsed_data["error"] != nil 
+
+      ###### make a post request to the moves api endpoint for refresh tokens  ######
+      uri = URI.parse("https://api.moves-app.com")
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Post.new("/oauth/v1/access_token?" + 
+        "grant_type=refresh_token&refresh_token=" + @patient.refresh_token + "&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl" +
+        "&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW")
+
+      @res = http.request(request)
+      @res = @res.body
+
+      ###### renew the access_token and refresh_token in the database ######
+      @patient.access_token = JSON.parse(@res)["access_token"]
+      @patient.refresh_token = JSON.parse(@res)["refresh_token"]
+      
+
+      ###### make a request for fitness data once again, using new access_token ######
+      access_token = @patient.access_token
+
+      uri = URI.parse("https://api.moves-app.com/api/1.1/user/activities/daily/" +
+        year_string + month_string + "?" + "access_token=" + access_token)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+
+      @body = response.body
+
+      @parsed_data = JSON.parse(@body)
+    end
 
 
   end
