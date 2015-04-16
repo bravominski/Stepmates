44afc0ee2aa9f999038c7181ebd5c1351b888503
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-16 11:18:46 -0400

    mailer error fix

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -2,7 +2,7 @@ class PatientMailer < ApplicationMailer
 	default from: @user.email
 
 	def welcome_email(patient)
-		@patient = patient
-		mail(:to @patient.email, subject:"From your doctor")
+		email = patient.email
+		mail(:to email, subject:"From your doctor")
 	end
 end
