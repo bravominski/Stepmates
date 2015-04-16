d8e379c4c5fe74f5971b6eb93324e624f026b01c
Jungi Kim <jungikim@sas.upenn.edu>
2015-04-15 23:25:43 -0400

    get monthly activities instead of daily

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -36,7 +36,7 @@ class PatientsController < ApplicationController
     access_token = @patient.access_token
 
     uri = URI.parse("https://api.moves-app.com/api/1.1/user/activities/daily/" +
-      year_string + month_string + day_string + "?" + "access_token=" + access_token)
+      year_string + month_string + "?" + "access_token=" + access_token)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(uri.request_uri)
