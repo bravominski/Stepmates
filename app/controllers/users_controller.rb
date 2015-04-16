bc632bdc39582a6c91ebac755d7fda747fe50afa
rextardus <minski@sas.upenn.edu>
2015-04-16 13:51:46 -0400

    disabling mailer temporarily

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        UserMailer.welcome_email(@user).deliver_now
+        #UserMailer.welcome_email(@user).deliver_now
         log_in @user
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
