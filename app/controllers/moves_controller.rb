21f22a3e0a9fd5b1c16c3c9ba90aa622584b2db3
Jinah Kim <kimja@seas.upenn.edu>
2015-04-16 16:55:29 -0400

    back to Jungi's Heroku

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -6,7 +6,7 @@ require 'net/https'
 class MovesController < ApplicationController
 	def getAuth
 		redirect_to "https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id=" +
-		 "7wwsaRMHVhYs2ab8Iz89OxwfigFI654M&scope=activity"
+		 "rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl&scope=activity"
 	end
 
 	def receiveAuth
@@ -19,7 +19,7 @@ class MovesController < ApplicationController
 		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 		request = Net::HTTP::Post.new("/oauth/v1/access_token?" + 
-			"grant_type=authorization_code&code=" + @code + "&client_id=7wwsaRMHVhYs2ab8Iz89OxwfigFI654M" +
+			"grant_type=authorization_code&code=" + @code + "&client_id=rk4jNeuJ054WTTlYV9l4QF9dYsGdFwdl" +
 			"&client_secret=gE8sVNO5MQNTX_tKLMoYtfBSu4QVeOco5x9FW_FSq38v0V14K_OKRTo69TSUpvhW")
 
 		@res = http.request(request)
