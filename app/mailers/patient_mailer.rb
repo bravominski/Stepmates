bdb81bf18798712cda980e5953a5e56fe3fb5550
rextardus <minski@sas.upenn.edu>
2015-04-17 01:20:47 -0400

    Adding back all the mailers

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -1,9 +1,16 @@
 class PatientMailer < ApplicationMailer
 
-	#def welcome_email(user, patient)
-	#	@patient = patient
-	#	@user = user
-	#	mail(from: @user.email, :to @patient.email, subject:"From your doctor")
-	#end
+	def welcome_email(user, patient)
+		@patient = patient
+		@user = user
+		mail(from: @user.email, to: @patient.email, subject:"From your doctor")
+	end
+
+	def custom_email(user, patient, message)
+		@patient = patient
+		@user = user
+		@message = message
+		mail(from: @user.email, to: @patient.email, subject:"From your doctor")
+	end
 
 end
