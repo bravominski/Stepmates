00c9476d5ded9115a59538832f88b658bf3ae6ad
Jinah Kim <kimja@seas.upenn.edu>
2015-04-16 23:39:42 -0400

    add # for case-insensitivity

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,10 +1,8 @@
 class Patient < ActiveRecord::Base
 
-	validates_uniqueness_of :name, :case_sensitive => false
-
 	def self.search(search)
   		if search
-    		where("name LIKE ?", "%#{search}%")
+    		where("name ILIKE ?", "%#{search}%")
   		else
     		Patient.all
     	end
