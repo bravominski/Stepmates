eb3ac7fbfe64a1559c162c56055c63c6afb72f6d
Jinah Kim <kimja@seas.upenn.edu>
2015-05-03 11:34:25 -0400

    search -- mrn search-- fixed

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ActiveRecord::Base
 
 	def self.search(search)
   		if search
-    		where('name ILIKE ?' || 'mrn ILIKE ?', "%#{search}%")
+    		where('name ILIKE ?' || 'mrn LIKE ?', "%#{search}%")
   		else
     		Patient.all
     	end
