ebc55bd19935d34b286ff04a0120d32b89900105
Jinah Kim <kimja@seas.upenn.edu>
2015-05-03 10:46:36 -0400

    search-- add mrn search

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ActiveRecord::Base
 
 	def self.search(search)
   		if search
-    		find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    		find(:all, :conditions => ['name LIKE ? OR mrn LIKE ?', "%#{search}%"])
   		else
     		Patient.all
     	end
