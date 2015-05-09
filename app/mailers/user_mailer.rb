8c619be60b737a8d00d2612f585db94afd18c27c
Jinah Kim <kimja@seas.upenn.edu>
2015-05-09 10:03:41 -0400

    last addition

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,6 +9,6 @@ class UserMailer < ApplicationMailer
 
 	def passwrod_reset(user)
 		@user = user
-		mail(to: @user.email, subject: "Here is your password!")
+		mail(from: forgotpassword@stepmates.com to: @user.email, subject: "Here is your password!")
 	end
 end
